4367a0cbceebb7907d09a776237845932221aab1
Pasindu19970808 <pasindu.samaranayake@yahoo.com>
2022-04-13 22:20:41 +0800

    hyperparameter tuning

--- pipeline_functionality.py
+++ pipeline_functionality.py
@@ -21,37 +21,51 @@ class preprocess_data():
     def __init__(self,**kwargs):
         self.filepath = kwargs["filepath"]
         self.scaler = None
-    def read_df(self,process_complete = False):
+    def read_df(self,process_complete = False,test_df = False,test_path = ""):
+        if test_df == True:
+            self.filepath = test_path
         df = pd.read_csv(self.filepath,encoding="ISO-8859-1")
         df.drop('id',axis = 1,inplace=True)
         df = self.rename_columns(df)
         if process_complete != False:
             #drop na
-            df = df.dropna(subset=["Feature 0","Feature 1","Feature 10"])
-            df = df.reset_index(drop=True)
-            oversample = SMOTE()
-            df_train,df_label = oversample.fit_resample(df[[i for i in df.columns.tolist() if i!="label"]],df[[i for i in df.columns.tolist() if i=="label"]])
-            df_train["labels"] = df_label
-            df = df_train
-            #remove highly correlated features
-            df = df.drop(['Feature 3', 'Feature 4', 'Feature 7','Age','Sex'],axis = 1)
-            feature_eng = feature_engineering()
-            df = feature_eng.feature0_fit_transform(df)
-            df = feature_eng.feature5_fit_transform(df)
-            df = feature_eng.feature10_fit_transform(df)
-            df = df.drop(["Feature 1","Feature 5","Feature 10"],axis = 1)
-            df = pd.concat([df[[i for i in df.columns.tolist() if "_" not in i and i != "labels"]],pd.get_dummies(df[['Feature 1_Cat','Feature 5_Cat','Feature 10_Cat',]]),df[["labels"]]],axis = 1)
-            self.scaler = StandardScaler()
-            self.scaler.fit(df[["Feature 0","Feature 2","Feature 6"]])
-            df[["Feature 0","Feature 2","Feature 6"]] = self.scaler.transform(df[["Feature 0","Feature 2","Feature 6"]])
+            if test_df == False:
+                df = df.dropna(subset=["Feature 0","Feature 1","Feature 10"])
+                df = df.reset_index(drop=True)
+                # oversample = SMOTE()
+                # df_train,df_label = oversample.fit_resample(df[[i for i in df.columns.tolist() if i!="label"]],df[[i for i in df.columns.tolist() if i=="label"]])
+                # df_train["labels"] = df_label
+                # df = df_train
+                #remove highly correlated features
+                # df = df.drop(['Feature 3', 'Feature 4', 'Feature 7','Age','Sex'],axis = 1)
+
+                self.feature_eng = feature_engineering()
+                df = self.feature_eng.feature0_fit_transform(df)
+                df = self.feature_eng.feature5_fit_transform(df)
+                df = self.feature_eng.feature10_fit_transform(df)
+            else:
+                df = self.feature_eng.transform_df(df)
+            df.loc[df["Sex"] == 1,"Sex"] = "M"
+            df.loc[df["Sex"] == 0,"Sex"] = "F"
+            #works for training df and not test df
+            try:
+                df = pd.concat([df[[i for i in df.columns.tolist() if "_" not in i and i != "labels"]],pd.get_dummies(df[['Feature 1_Cat','Feature 5_Cat','Feature 10_Cat',"Sex"]]),df[["labels"]]],axis = 1)
+            except:
+                #works for test df
+                df = pd.concat([df[[i for i in df.columns.tolist() if "_" not in i and i != "labels"]],pd.get_dummies(df[['Feature 1_Cat','Feature 5_Cat','Feature 10_Cat',"Sex"]])],axis = 1)
+            df = df.drop(["Feature 1","Feature 5","Feature 10","Sex"],axis = 1)
+
+            if test_df == False:
+                self.scaler = StandardScaler()
+                self.scaler.fit(df[["Feature 0","Feature 2","Feature 6",'Feature 3', 'Feature 4', 'Feature 7','Age']])
+            df[["Feature 0","Feature 2","Feature 6",'Feature 3', 'Feature 4', 'Feature 7','Age']] = self.scaler.transform(df[["Feature 0","Feature 2","Feature 6",'Feature 3', 'Feature 4', 'Feature 7','Age']])
         return df
+        
         # if split == True:
         #     df_labels = df.loc[:,"label"]
         #     df_train = df.loc[:,[i for i in df.columns.tolist() if i != "label"]]
         #     return df_train,df_labels
         # else:
-            
-
 
     #Rename columns for easier reading
     def rename_columns(self,df:pd.DataFrame):
@@ -87,14 +101,14 @@ class feature_engineering:
         self.feature10boundary = None
     def feature0_fit_transform(self,df):
         if (self.feature1boundary == None) and ("Feature1_Cat" not in df.columns.tolist()):
-            self.feature1boundary = df.loc[df["labels"] == 0]["Feature 1"].describe()["75%"] + 1.5*(df.loc[df["labels"] == 0]["Feature 1"].describe()["75%"] - df.loc[df["labels"] == 0]["Feature 1"].describe()["25%"])
+            self.feature1boundary = df.loc[df["label"] == 0]["Feature 1"].describe()["75%"] + 1.5*(df.loc[df["label"] == 0]["Feature 1"].describe()["75%"] - df.loc[df["label"] == 0]["Feature 1"].describe()["25%"])
             df = self.apply_change(df,"Feature 1",{"LTE":"Low Risk","GT":"High Risk"},self.feature1boundary)
             return df
         else:
             raise Warning("Feature 0 boundary to split categorical variable is already set. No change to the data was done")
     def feature5_fit_transform(self,df):
         if (self.feature5boundary == None) and ("Feature5_Cat" not in df.columns.tolist()):
-            self.feature5boundary = df.loc[df["labels"] == 1]["Feature 5"].describe()["75%"]
+            self.feature5boundary = df.loc[df["label"] == 1]["Feature 5"].describe()["75%"]
             df = self.apply_change(df,"Feature 5",{"LTE":"High Risk","GT":"Low Risk"},self.feature5boundary)
             # df.loc[df["Feature 5"] <= self.feature5boundary,'Feature5_Cat'] = "High Risk"
             # df.loc[df["Feature 5"] > self.feature5boundary,'Feature5_Cat'] = "Low Risk"
@@ -103,7 +117,7 @@ class feature_engineering:
             raise Warning("Feature 5 boundary to split categorical variable is already set. No change to the data was done")
     def feature10_fit_transform(self,df):
         if (self.feature10boundary == None) and ("Feature10_Cat" not in df.columns.tolist()):
-            self.feature10boundary = df.loc[df["labels"] == 0]["Feature 10"].describe()["75%"]
+            self.feature10boundary = df.loc[df["label"] == 0]["Feature 10"].describe()["75%"]
             df = self.apply_change(df,"Feature 10",{"LTE":"Low Risk","GT":"High Risk"},self.feature10boundary)
             # df.loc[df["Feature 10"] <= self.feature10boundary,'Feature10_Cat'] = "Low Risk"
             # df.loc[df["Feature 10"] > self.feature10boundary,'Feature10_Cat'] = "High Risk"
@@ -115,7 +129,7 @@ class feature_engineering:
         df = self.apply_change(df,"Feature 1",{"LTE":"Low Risk","GT":"High Risk"},self.feature1boundary)
         df = self.apply_change(df,"Feature 5",{"LTE":"High Risk","GT":"Low Risk"},self.feature5boundary)
         df = self.apply_change(df,"Feature 10",{"LTE":"Low Risk","GT":"High Risk"},self.feature10boundary)
-
+        return df
     def apply_change(self,df,col,change_dict,boundary):
         catcol = col + "_Cat"
         df[catcol] = np.nan
@@ -140,7 +154,8 @@ class NeuralNetwork(nn.Module):
         self.activation_layers = {
         'relu':nn.ReLU(),
         'sigmoid':nn.Sigmoid(),
-        'tanh':nn.Tanh()
+        'tanh':nn.Tanh(),
+        'elu':nn.ELU(),
         }
         try:
             Type_Checks(layer_info_dict = layer_info_dict)
@@ -168,7 +183,7 @@ class TrainingValidatingNetwork:
         model.train()
         for batch,(X,Y) in enumerate(dataloader):
             pred = model(X.float())
-            loss = loss_fn(pred,Y.unsqueeze(0).float())
+            loss = loss_fn(pred,Y.float())
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -180,7 +195,7 @@ class TrainingValidatingNetwork:
         with torch.no_grad():
             for batch,(X,Y) in enumerate(dataloader):
                 y_pred = model(X.float())
-                loss = loss_fn(y_pred,Y.unsqueeze(0).float())
+                loss = loss_fn(y_pred,Y.float())
                 testing_loss += loss.item()
                 validate_inst_count += 1
                 #y_pred.argmax(1) == y will return True or False
@@ -189,18 +204,22 @@ class TrainingValidatingNetwork:
                 #.astype(torch.float) makes it to a 1 or 0
                 #As its one instance at a time, no need to sum
                 #output y_pred is of shape [1], y is of shape[]
-                correct += (y_pred.round() == Y).type(torch.float).item()
+                correct += (y_pred.round() == Y.float()).type(torch.float).item()
         #calculate average loss across the batches
         avg_loss = testing_loss/validate_inst_count
         #calculate average accuracy across the batches
         avg_accuracy = correct/validate_inst_count
-        return avg_loss,avg_accuracy
-    def train_and_validate(self,train_dataloader,validate_dataloader,model,loss_fn,epochs,optimizer):
+        return avg_accuracy,avg_loss,model
+    def train_and_validate(self,train_dataloader,validate_dataloader,model,loss_fn,epochs,optimizer,predicting = False):
         self.optimizer = optimizer
         for i in range(epochs):
             self.train(train_dataloader,model,loss_fn,self.optimizer)
-        avg_accuracy,avg_loss = self.validate(validate_dataloader,model,loss_fn)
-        return avg_accuracy,avg_loss
+        if predicting == False:
+            avg_accuracy,avg_loss,model = self.validate(validate_dataloader,model,loss_fn)
+            return avg_accuracy,avg_loss,model
+        else:
+            #If we are only trying to predict for Submission purposes, no validation will be done
+            return model
 
 
 
